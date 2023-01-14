b031c51f5192337f45b8a5e387afee2bfc58e3df
Jonas <84239673+Joensw@users.noreply.github.com>
2023-01-14 17:15:06 +0100

    Add strictMapLib.sol to experimental

---
+++ experimental/strictMapLib.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.7.0;
+
+library StrictMap {
+
+    struct Strictmapping {
+        mapping (address => uint) map;
+    }
+
+    /// @notice postcondition contents == self.map[key]
+
+    function get(Strictmapping storage self, address key) public view returns (uint contents) {
+        contents = self.map[key];
+    }
+
+    /// @notice postcondition forall (address a) !(a != msg.sender ) || (self.map[a] == __verifier_old_uint(self.map[a]))
+    /// @notice postcondition self.map[msg.sender] == value
+
+    function set(Strictmapping storage self, uint value) public {
+        self.map[msg.sender] = value;
+    }
+
+    /// @notice postcondition forall (address a) !(a != target) || (self.map[a] == __verifier_old_uint(self.map[a]))
+    /// @notice postcondition self.map[target] == __verifier_old_uint(self.map[target]) + amount
+
+    function deposit(Strictmapping storage self, address target, uint amount) public {
+        self.map[target] += amount;
+    }
+
+}
