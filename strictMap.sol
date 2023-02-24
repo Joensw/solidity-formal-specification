e0949f9a494be5c63a1b3f580dcc7b4038c15d60
Jonas <84239673+Joensw@users.noreply.github.com>
2023-02-24 16:58:31 +0100

    Change StrictMapping to CallerAddressableMapping

--- strictMap.sol
+++ strictMap.sol
@@ -1,28 +1,28 @@
 pragma solidity ^0.7.0;
 
-library StrictMaps {
+library CallerAddressableMappings {
 
-    struct StrictMap {
+    struct CallerAddressableMapping {
         mapping (address => uint) _map;
     }
 
     /// @notice postcondition contents == self._map[key]
 
-    function get(StrictMap storage self, address key) public view returns (uint contents) {
+    function get(CallerAddressableMapping storage self, address key) public view returns (uint contents) {
         contents = self._map[key];
     }
 
     /// @notice postcondition forall (address a) !(a != msg.sender ) || (self._map[a] == __verifier_old_uint(self._map[a]))
     /// @notice postcondition self._map[msg.sender] == value
 
-    function set(StrictMap storage self, uint value) public {
+    function set(CallerAddressableMapping storage self, uint value) public {
         self._map[msg.sender] = value;
     }
 
     /// @notice postcondition forall (address a) !(a != target) || (self._map[a] == __verifier_old_uint(self._map[a]))
     /// @notice postcondition self._map[target] == __verifier_old_uint(self._map[target]) + amount
 
-    function deposit(StrictMap storage self, address target, uint amount) public {
+    function deposit(CallerAddressableMapping storage self, address target, uint amount) public {
         self._map[target] += amount;
     }
 
