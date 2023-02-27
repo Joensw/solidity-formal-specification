ee1a466d63d6c655cb1151d7b03555adb76fb2a0
Jonas <84239673+Joensw@users.noreply.github.com>
2023-02-27 15:32:47 +0100

    Introduce generic iterable mapping file

---
+++ experimental/genericIterableMapping.sol
@@ -0,0 +1,204 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+// The first 6 lines of every method represent the following invariants:
+// invariant forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+// invariant forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+// invariant forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+
+library IterableMappingIterator {
+
+    struct Iterator {
+        uint index;
+        %TYPE_KEY%[] items;
+    }
+ 
+    // For testing purposes, can be ignored
+
+    function get(Iterator storage self) public view returns (%TYPE_KEY%[] memory) {
+        return self.items;
+    }
+
+    /// @notice postcondition self.index == 0
+    /// @notice postcondition self.items.length == itmap.keys.length
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.items.length) || self.items[i] == itmap.keys[i]
+
+    function init(Iterator storage self, IterableMappings.IterableMapping storage itmap) public {
+        self.items = itmap.keys;
+        self.index = 0;
+    }
+
+    /// @notice postcondition !ret || self.index < self.items.length
+    /// @notice postcondition ret || self.index >= self.items.length
+
+    function hasNext(Iterator storage self) public view returns (bool ret) {
+        return self.index < self.items.length;
+    }
+
+    /// @notice postcondition item == __verifier_old_%TYPE_KEY%(self.items[self.index])
+    /// @notice postcondition self.index == __verifier_old_uint(self.index) + 1
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.items.length) || self.items[i] == __verifier_old_%TYPE_KEY%(self.items[i])
+
+    function next(Iterator storage self) public returns (%TYPE_KEY% item) {
+        item = self.items[self.index];
+        self.index++;
+    }
+}
+
+library IterableMappings {
+    
+    // Should be 0 for uint/int, false for bool and 0x0000000000000000000000000000000000000000 for address
+    %TYPE_VALUE% private constant defaultValue = 0;
+
+    struct IterableMapping {
+        %TYPE_KEY%[] keys;
+        mapping (%TYPE_KEY% => %TYPE_VALUE%) values;
+        mapping (%TYPE_KEY% => uint) indexOf;
+    }
+
+    // For testing purposes, can be ignored
+
+    function get(IterableMapping storage self) public view returns (%TYPE_KEY%[] memory) {
+        return self.keys;
+    }
+
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice precondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition value == self.values[_key]
+    /// @notice postcondition !(self.indexOf[_key] == 0) || (value == defaultValue)
+
+    function get(IterableMapping storage self, %TYPE_KEY% _key) public view returns (%TYPE_VALUE% value) {
+        return self.values[_key];
+    }
+
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice precondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition out == self.keys.length
+
+    function size(IterableMapping storage self) public view returns (uint out) {
+        return self.keys.length;
+    }
+
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice precondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition exists (uint i) !out || !(0 <= i && i < self.keys.length) || self.keys[i] == _key
+    /// @notice postcondition forall (uint i) out || !(0 <= i && i < self.keys.length) || self.keys[i] != _key
+
+    function containsKey(IterableMapping storage self, %TYPE_KEY% _key) public view returns (bool out) {
+        return self.indexOf[_key] != 0;
+    }
+
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice precondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition self.values[_key] == _value
+    /// @notice postcondition forall (%TYPE_KEY% a) !(a != _key) || self.values[a] == __verifier_old_%TYPE_VALUE%(self.values[a])
+    /// @notice postcondition !(__verifier_old_uint(self.indexOf[_key]) == 0) || (self.keys.length == __verifier_old_uint(self.keys.length) + 1)
+    /// @notice postcondition !(__verifier_old_uint(self.indexOf[_key]) == 0) || (self.keys[self.keys.length - 1] == _key)
+    /// @notice postcondition !(__verifier_old_uint(self.indexOf[_key]) == 0) || (self.indexOf[_key] == self.keys.length)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(__verifier_old_uint(self.indexOf[_key]) == 0) || !(a != _key) || self.indexOf[a] == __verifier_old_uint(self.indexOf[a])
+    /// @notice postcondition forall (uint i) !(__verifier_old_uint(self.indexOf[_key]) == 0) || !(0 <= i && i < self.keys.length - 1) || self.keys[i] == __verifier_old_%TYPE_KEY%(self.keys[i])
+    /// @notice postcondition forall (%TYPE_KEY% a) (__verifier_old_uint(self.indexOf[_key]) == 0) || self.indexOf[a] == __verifier_old_uint(self.indexOf[a])
+    /// @notice postcondition forall (uint i) (__verifier_old_uint(self.indexOf[_key]) == 0) || !(0 <= i && i < self.keys.length) || self.keys[i] == __verifier_old_%TYPE_KEY%(self.keys[i])
+
+    function set(IterableMapping storage self, %TYPE_KEY% _key, %TYPE_VALUE% _value) public {
+        self.values[_key] = _value;
+
+        if (self.indexOf[_key] == 0) {
+            self.keys.push(_key);
+            self.indexOf[_key] = self.keys.length;
+        }
+    }
+
+    /*
+
+        ensures: success <=> (__verifier_old_uint(self.keys.length) != 0 && __verifier_old_uint(self.indexOf[_key]) != 0)
+            =>
+            <=
+
+        ensures: success => self.keys.length == __verifier_old_uint(self.keys.length) - 1
+        ensures: !success => self.keys.length == __verifier_old_uint(self.keys.length)
+
+        ensures !success => all data strucutres remain unchanged
+            keys
+            values
+            indexOf
+
+        ensures success =>
+            values _key is now 0 && indexof _key is now 0
+            all entries in keys are != _key, i.e _key doesnt appear in _keys anymore
+            values stays the same except for _key 
+
+        ensures: success & last => 
+            keys before is the same as keys after just one shorter
+            indexOf stays the same except for _key
+
+        ensures: success & !last => 
+            The old last element is now at the index where _key was
+                in keys
+                in indexOF
+            everything else stays the same
+                in keys (except ofcourse the last element is cut)
+                in indexOF
+
+
+    
+    */
+
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] != 0) || (self.indexOf[a] - 1 < self.keys.length) && (self.keys[self.indexOf[a] - 1] == a)
+    /// @notice precondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice postcondition forall (uint i) !(0 <= i && i < self.keys.length) || (self.indexOf[self.keys[i]] - 1 == i)
+    /// @notice precondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition forall (%TYPE_KEY% a) !(self.indexOf[a] == 0) || (self.values[a] == defaultValue)
+    /// @notice postcondition success || (__verifier_old_uint(self.keys.length) == 0 || __verifier_old_uint(self.indexOf[_key]) == 0)
+    /// @notice postcondition !success || (__verifier_old_uint(self.keys.length) != 0 && __verifier_old_uint(self.indexOf[_key]) != 0)
+    /// @notice postcondition success || self.keys.length == __verifier_old_uint(self.keys.length)
+    /// @notice postcondition !success || self.keys.length == __verifier_old_uint(self.keys.length) - 1
+    /// @notice postcondition forall (uint i) success || (0 <= i && i < self.keys.length) || self.keys[i] == __verifier_old_%TYPE_KEY%(self.keys[i])
+    /// @notice postcondition forall (%TYPE_KEY% a) success || self.values[a] == __verifier_old_%TYPE_VALUE%(self.values[a])
+    /// @notice postcondition forall (%TYPE_KEY% a) success || self.indexOf[a] == __verifier_old_uint(self.indexOf[a])
+    /// @notice postcondition !success || self.values[_key] == defaultValue && self.indexOf[_key] == 0
+    /// @notice postcondition forall (uint i) !success || !(0 <= i && i < self.keys.length) || self.keys[i] != _key
+    /// @notice postcondition forall (%TYPE_KEY% a) !success || !(a != _key) || self.values[a] == __verifier_old_%TYPE_VALUE%(self.values[a])
+    /// @notice postcondition forall (uint i) !success || !(_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])) || !(0 <= i && i < self.keys.length) || self.keys[i] == __verifier_old_%TYPE_KEY%(self.keys[i])
+    /// @notice postcondition forall (%TYPE_KEY% a) !success || !(_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])) || !(a != _key) || self.indexOf[a] == __verifier_old_uint(self.indexOf[a])
+    /// @notice postcondition !success || (_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])) || (__verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1]) == self.keys[__verifier_old_uint(self.indexOf[_key] - 1)])
+    /// @notice postcondition !success || (_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])) || self.indexOf[__verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])] == __verifier_old_uint(self.indexOf[_key])
+    /// @notice postcondition forall (uint i) (!success) || (_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1]))  || !(0 <= i && i < self.keys.length && i != __verifier_old_uint(self.indexOf[_key] - 1)) || (self.keys[i] == __verifier_old_%TYPE_KEY%(self.keys[i]))
+    /// @notice postcondition forall (%TYPE_KEY% a) (!success) || (_key == __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1])) || !(a != __verifier_old_%TYPE_KEY%(self.keys[self.keys.length - 1]) && a != _key) || (self.indexOf[a] == __verifier_old_uint(self.indexOf[a]))
+
+    function remove(IterableMapping storage self, %TYPE_KEY% _key) public returns (bool success) {
+        if (self.keys.length == 0) return false;
+        if (self.indexOf[_key] == 0) return false;
+    
+
+        // move last elemet of keys array to where the element we want to remove was
+        uint index = self.indexOf[_key] - 1;
+        %TYPE_KEY% lastKey = self.keys[self.keys.length - 1];
+        self.keys[index] = lastKey;
+        self.indexOf[lastKey] = index + 1;
+
+        delete self.values[_key];
+        delete self.indexOf[_key];
+
+        self.keys.pop();
+        return true;
+    }
+
+}
--- experimental/replacer.py
+++ experimental/replacer.py
@@ -1,17 +1,19 @@
 import sys
 
-
-def replace(fileName, keyword: str, replacement: str):
+def replace(fileName, keyword_1: str, replacement_1: str, keyword_2 : str = "", replacement_2 : str = ""):
     fIn = open(fileName, "rt")
-    fOut = open(f"{replacement}_" + fileName, "wt")
+    fOut = open(f"{replacement_1}L{replacement_2}_" + fileName, "wt")
     for line in fIn:
-        fOut.write(line.replace(keyword, replacement))
+        fOut.write(line.replace(keyword_1, replacement_1).replace(keyword_2, replacement_2))
     fIn.close()
     fOut.close()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4 or len(sys.argv) > 6 :
         print(f"Incorrect number of arguments: {sys.argv} needed are fileName, keyword, replacement")
         exit(0)
-    replace(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) == 6 :
+        replace(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    else :
+        replace(sys.argv[1], sys.argv[2], sys.argv[3])
