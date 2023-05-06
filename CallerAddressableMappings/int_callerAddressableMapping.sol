c394f8f0db7be9b1c0a73cb6817b142f0f71c4ff
Jonas <84239673+Joensw@users.noreply.github.com>
2023-05-06 15:08:16 +0200

    Rework csm deposite functions

--- CallerAddressableMappings/int_callerAddressableMapping.sol
+++ CallerAddressableMappings/int_callerAddressableMapping.sol
@@ -26,21 +26,20 @@ library CallerAddressableMappings {
         self.map[msg.sender] = value;
     }
 
-    /// @notice postcondition forall (address a) !(a != target) || (self.map[a] == __verifier_old_int(self.map[a]))
-    /// @notice postcondition self.map[target] == __verifier_old_int(self.map[target]) + amount
-    /// @notice postcondition self.map[target] >= __verifier_old_int(self.map[target])
+    /// @notice postcondition !(amount < 0 || __verifier_old_int(self.map[msg.sender]) < amount || target == msg.sender) || !success
+    /// @notice postcondition forall (address a) !(a != target && a != msg.sender) || (self.map[a] == __verifier_old_int(self.map[a]))
+    /// @notice postcondition forall (address a) success || (self.map[a] == __verifier_old_int(self.map[a]))
+    /// @notice postcondition !success || self.map[target] == __verifier_old_int(self.map[target]) + amount
+    /// @notice postcondition !success || self.map[target] >= __verifier_old_int(self.map[target])
+    /// @notice postcondition !success || self.map[msg.sender] ==  __verifier_old_int(self.map[msg.sender]) - amount
 
-    function depositTarget(CallerAddressableMapping storage self, address target, int amount) public {
-        require(amount >= 0);
-        self.map[target] += amount;
+    function transfer(CallerAddressableMapping storage self, address target, int amount) public returns (bool success) {
+        if (amount < 0 || self.map[msg.sender] < amount || target == msg.sender) {
+            return false;
+        }
+        self.map[msg.sender] -= amount;
+        self.map[target] += amount;  
+        return true;  
     }
 
-    /// @notice postcondition forall (address a) !(a != msg.sender) || (self.map[a] == __verifier_old_int(self.map[a]))
-    /// @notice postcondition self.map[msg.sender] == __verifier_old_int(self.map[msg.sender]) + amount
-    /// @notice postcondition self.map[msg.sender] >= __verifier_old_int(self.map[msg.sender])
-
-    function depositSelf(CallerAddressableMapping storage self, int amount) public {
-        require(amount >= 0);
-        self.map[msg.sender] += amount;
-    }
 }
